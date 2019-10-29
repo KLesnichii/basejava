8ad398ee01187a9a6a7a40ac8454b23afc7ebb5f
due9 <79299288612@yandex.ru>
2019-10-29 23:10:28 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int counter = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        counter = 0;
     }
 
     void save(Resume r) {
+        storage[counter] = r;
+        counter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                if (i < counter - 1) System.arraycopy(storage, i + 1, storage, i, counter - i - 1);
+                counter--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, counter);
     }
 
     int size() {
-        return 0;
+        return counter;
     }
 }
