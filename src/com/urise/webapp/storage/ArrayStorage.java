b6dae26a64a59ddfd76f8a04abcebe33db0250de
due9 <79299288612@yandex.ru>
2019-11-06 01:04:25 +0300

    HW2

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_SIZE = 10000;
+    private Resume[] storage = new Resume[STORAGE_SIZE];
     private int counter = 0;
 
     public void clear() {
@@ -17,32 +18,49 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        storage[counter] = r;
-        counter++;
+        int index = searchResume(r.getUuid());
+        //check Overflow
+        if (counter == STORAGE_SIZE) {
+            System.out.println("Resume save OverflowError");
+            return;
+        }
+        if (index == -1) {
+            storage[counter] = r;
+            counter++;
+        } else {
+            System.out.println("Resume save Error");
+        }
     }
 
     public void update(Resume r) {
-        storage[counter] = r;
-        counter++;
+        int index = searchResume(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume update Error");
+        } else {
+            storage[index] = r;
+        }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < counter; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        int index = searchResume(uuid);
+        if (index == -1) {
+            System.out.println("Resume get Error");
+            return null;
         }
-        return null;
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < counter; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, counter - i - 1);
-                counter--;
-                storage[counter] = null;
-                break;
+        int index = searchResume(uuid);
+        if (index == -1) {
+            System.out.println("Resume delete Error");
+        } else {
+            //check ArrayIndexOutOfBounds
+            if (index != STORAGE_SIZE - 1) {
+                System.arraycopy(storage, index + 1, storage, index, counter - index - 1);
             }
+            counter--;
+            storage[counter] = null;
         }
     }
 
@@ -55,5 +73,18 @@ public class ArrayStorage {
 
     public int size() {
         return counter;
+    }
+
+    /**
+     * @return index of the uuid, if it is contained in storage;
+     * otherwise return -1;
+     */
+    private int searchResume(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
