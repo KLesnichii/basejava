709aa03635c6495747bb1a1ede5ffe67a917aaf3
KLesnichii <due9@yandex.ru>
2020-01-25 01:49:52 +0300

    Lesson 12

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,8 +1,15 @@
 package ru.javawebinar.basejava;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MainConcurrency {
     private static int counter;
+    private static final AtomicInteger atomicCounter = new AtomicInteger();
     private final static Object LOCK = new Object();
+    private static final Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -19,23 +26,46 @@ public class MainConcurrency {
 
         System.out.println(thread0.getState());
 
+//        for (int i = 0; i < 10_000; i++) {
+//            new Thread(() -> {
+//                for (int j = 0; j < 100; j++) {
+//                    inc();
+//                }
+//            }).start();
+//        }
+//
+//        Thread.sleep(500);
+
+        CountDownLatch latch = new CountDownLatch(10_000);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         for (int i = 0; i < 10_000; i++) {
-            new Thread(() -> {
+            executorService.submit(() -> {
                 for (int j = 0; j < 100; j++) {
                     inc();
                 }
-            }).start();
+                latch.countDown();
+            });
         }
 
-        Thread.sleep(500);
-        System.out.println(counter);
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        System.out.println(atomicCounter.get());
+
     }
 
     private static void inc() {
-        double a = Math.sin(13);
-        synchronized (LOCK) {
-            counter++;
-        }
+//      lock.lock();
+//      try {
+//          counter++;
+//      } finally {
+//          lock.unlock();
+//      }
+        atomicCounter.incrementAndGet();
     }
+
+
+
+
+
 }
 
